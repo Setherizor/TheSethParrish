c9ac47fca40be995737a8fda7d3d08977c2d1cd7
shparri2 <shparri2@ncsu.edu>
2017-11-18 16:03:48 -0500

    Misc Fixes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TheSethParrish
+A little webapp
--- public/sw.js
+++ public/sw.js
@@ -1,75 +1,75 @@
-var CACHE_NAME = 'my-site-cache-v1'
+var CACHE_NAME = 'TheSethParrishCache'
 var urlsToCache = [
-    // '/',
-    // '/index.html',
-    // 'css/style.css',
-    // 'css/nav.css',
-    // '/index.js'
+  // '/',
+  // '/index.html',
+  // 'css/style.css',
+  // 'css/nav.css',
+  // '/index.js'
 ]
 
 // Perform install steps
 self.addEventListener('install', function (event) {
-    // Perform install steps
+  // Perform install steps
   event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-              console.log('Opened cache')
-              return cache.addAll(urlsToCache)
-            })
-    )
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache')
+        return cache.addAll(urlsToCache)
+      })
+  )
 })
 
 self.addEventListener('activate', function (event) {
   var cacheWhitelist = ['NONE']
   event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-          return Promise.all(
-                cacheNames.map(function (cacheName) {
-                  if (cacheWhitelist.indexOf(cacheName) === -1) {
-                    return caches.delete(cacheName)
-                  }
-                })
-            )
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName)
+          }
         })
-    )
+      )
+    })
+  )
 })
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-        caches.match(event.request).then(function (response) {
-            // Cache hit - return response
-          if (response) {
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response
+      }
+
+      // IMPORTANT: Clone the request. A request is a stream and
+      // can only be consumed once. Since we are consuming this
+      // once by cache and once by the browser for fetch, we need
+      // to clone the response.
+      var fetchRequest = event.request.clone()
+
+      return fetch(fetchRequest).then(
+        function (response) {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
             return response
           }
 
-            // IMPORTANT: Clone the request. A request is a stream and
-            // can only be consumed once. Since we are consuming this
-            // once by cache and once by the browser for fetch, we need
-            // to clone the response.
-          var fetchRequest = event.request.clone()
+          // IMPORTANT: Clone the response. A response is a stream
+          // and because we want the browser to consume the response
+          // as well as the cache consuming the response, we need
+          // to clone it so we have two streams.
+          var responseToCache = response.clone()
 
-          return fetch(fetchRequest).then(
-                function (response) {
-                    // Check if we received a valid response
-                  if (!response || response.status !== 200 || response.type !== 'basic') {
-                    return response
-                  }
-
-                    // IMPORTANT: Clone the response. A response is a stream
-                    // and because we want the browser to consume the response
-                    // as well as the cache consuming the response, we need
-                    // to clone it so we have two streams.
-                  var responseToCache = response.clone()
-
-                  caches.open(CACHE_NAME)
-                        .then(function (cache) {
-                          cache.put(event.request, responseToCache)
-                        })
-                  return response
-                }
-            )
-        })
-    )
+          caches.open(CACHE_NAME)
+            .then(function (cache) {
+              cache.put(event.request, responseToCache)
+            })
+          return response
+        }
+      )
+    })
+  )
 })
 
 // Database based notifications
@@ -106,11 +106,11 @@ firebase.database().ref('notify/').on('value', function (snapshot) {
       tag: 'vibration-sample-Seff'
     })
 
-        // Set sent to false
+    // Set sent to false
     firebase.database().ref('notify/send').set('false')
     console.log('SW notification sent')
   } else if (val.send !== 'false') {
-        // Data validation
+    // Data validation
     firebase.database().ref('notify/send').set('false')
   }
 })
@@ -119,21 +119,21 @@ self.addEventListener('notificationclick', function (event) {
   let url = URL
   event.notification.close() // Android needs explicit close.
   event.waitUntil(
-        clients.matchAll({
-          type: 'window'
-        }).then(windowClients => {
-            // Check if there is already a window/tab open with the target URL
-          for (var i = 0; i < windowClients.length; i++) {
-            var client = windowClients[i]
-                // If so, just focus it.
-            if (client.url === url && 'focus' in client) {
-              return client.focus()
-            }
-          }
-            // If not, then open the target URL in a new window/tab.
-          if (clients.openWindow) {
-            return clients.openWindow(url)
-          }
-        })
-    )
+    clients.matchAll({
+      type: 'window'
+    }).then(windowClients => {
+      // Check if there is already a window/tab open with the target URL
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i]
+        // If so, just focus it.
+        if (client.url === url && 'focus' in client) {
+          return client.focus()
+        }
+      }
+      // If not, then open the target URL in a new window/tab.
+      if (clients.openWindow) {
+        return clients.openWindow(url)
+      }
+    })
+  )
 })
